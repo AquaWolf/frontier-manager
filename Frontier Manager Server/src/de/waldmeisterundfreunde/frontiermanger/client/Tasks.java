b93b7e1522293441db9fa1f8fc26f52128aff529
AquaWolf <ph.neumann@tu-bs.de>
2013-05-26 13:57:26 +0200

    implemented onLoad

--- Frontier Manager Server/src/de/waldmeisterundfreunde/frontiermanger/client/Tasks.java
+++ Frontier Manager Server/src/de/waldmeisterundfreunde/frontiermanger/client/Tasks.java
@@ -1,13 +1,59 @@
 package de.waldmeisterundfreunde.frontiermanger.client;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.zip.GZIPInputStream;
+
+import javax.xml.parsers.SAXParser;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
 import com.google.gwt.core.client.EntryPoint;
+
+import de.waldmeisterundfreunde.frontiermanger.parser.BlutrauschWarMapContentHandler;
 
 public class Tasks implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
-		
+		URL sourceURL;
+		try {
+			//prepare HTTP request
+//			sourceURL = new URL("http://www.allianzdesblutes.de/tnt/wmap/");
+//			HttpURLConnection sourceConnection = (HttpURLConnection) sourceURL.openConnection();
+//			sourceConnection.setRequestProperty("Accept-Encoding", "gzip");
+//			sourceConnection.connect();
+//			InputStream resultingInputStream = null;
+//			resultingInputStream = new GZIPInputStream(sourceConnection.getInputStream());
+//			InputSource inputSource = new InputSource(resultingInputStream);
+			
+			FileReader fileReader = new FileReader("test.html");
+			InputSource inputSource = new InputSource(fileReader);
+			
+			
+			XMLReader reader = XMLReaderFactory.createXMLReader();
+			reader.setContentHandler(new BlutrauschWarMapContentHandler());
+			reader.parse(inputSource);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//now the stream can be read directly,
+		//and the data will be on the contentType received above.
 	}
 
 }
