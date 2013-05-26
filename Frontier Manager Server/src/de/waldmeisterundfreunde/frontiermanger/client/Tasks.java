34f4741f1b59bf773c0452880e10605f249a0286
Dirk 2.0 <me-at-last@gmx.de>
2013-05-26 12:49:00 +0200

    added new site and contenthandler

---
+++ Frontier Manager Server/src/de/waldmeisterundfreunde/frontiermanger/client/Tasks.java
@@ -0,0 +1,13 @@
+package de.waldmeisterundfreunde.frontiermanger.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Tasks implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
